5d7ae2e6dc9bef9e400dc738fd9d17e49374c201
Dhanesha <dhaneshaudayangani@gmail.com>
2025-11-04 16:22:41 +0530

    Add ThemeProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import App from './App';
+import theme from './theme';
 import './index.css';
-import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
+      <ThemeProvider theme={theme}>
+      <CssBaseline />
       <App />
+    </ThemeProvider>
     </BrowserRouter>
 );
